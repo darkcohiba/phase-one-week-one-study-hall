0c6ff7f7baef41602ad76c16364975af172191e9
Sam <samuel.waters@gmail.com>
2022-11-01 14:36:35 -0600

    second

--- solutions.js
+++ solutions.js
@@ -30,7 +30,6 @@ function isEven(a){
     }
 }
 
-isEven(4)
 
 
 
@@ -46,13 +45,12 @@ function sumArray(a){
 // console.log(sumArray([1,2,3,4,5,6,7,8,9,10]));
 
 function fizzBuzz(a){
+    const array =[]
     for (var i = 1; i < a; i++) {
         if (i % 15 == 0) c("FizzBuzz");
-        else if (i % 3 == 0) c("Fizz");
-        else if (i % 5 == 0) c("Buzz");
-        else c(i);
+        else if (i % 3 == 0) array.push("Fizz");
+        else if (i % 5 == 0) array.push("Buzz");
+        else array.push(i);
     }
+    console.log(array);
 }
-
-// fizzBuzz(100);
-
